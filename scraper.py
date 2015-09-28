2624f704ad7b67eb9894450c8d5695068df56ae3
Andy Lulham <a.lulham@gmail.com>
2015-09-28 12:14:46 +0100

    Convert to python 2 (something like this…)

--- scraper.py
+++ scraper.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 import csv
-from io import StringIO
+from StringIO import StringIO
 import time
 
 import requests
@@ -13,7 +12,7 @@ j = requests.get(countries_url).json()
 
 tmpl = 'https://raw.githubusercontent.com/everypolitician/everypolitician-data/{sha}/{path}'
 for country in j:
-    print(country['name'])
+    print(country['name'].encode('utf8'))
     for legislature in country['legislatures']:
         leg_handles = {}
         for period in legislature['legislative_periods']:
@@ -21,15 +20,15 @@ for country in j:
                 sha=legislature['sha'],
                 path=period['csv'])).text
             time.sleep(0.5)
-            reader = csv.DictReader(StringIO(data))
+            reader = csv.DictReader(StringIO(data.encode('utf8')))
             for x in reader:
                 if x['twitter'] == '':
                     continue
                 leg_handles[x['id']] = {
-                    'person_id': x['id'],
-                    'country_code': country['code'],
-                    'legislature_slug': legislature['slug'],
-                    'twitter_handle': x['twitter'],
+                    'person_id': x['id'].decode('utf8'),
+                    'country_code': country['code'].decode('utf8'),
+                    'legislature_slug': legislature['slug'].decode('utf8'),
+                    'twitter_handle': x['twitter'].decode('utf8'),
                 }
         if leg_handles:
             scraperwiki.sqlite.save(["person_id", "country_code", "legislature_slug"], leg_handles.values(), "data")
