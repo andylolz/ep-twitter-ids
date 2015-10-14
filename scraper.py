d5eed288efb0283230d37f9b10d2ad927bc49637
Andy Lulham <a.lulham@gmail.com>
2015-10-14 13:26:00 +0100

    Add a comment about sqlite db name hack

--- scraper.py
+++ scraper.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 import csv
-import os
-os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 from io import StringIO
 import time
+import os
+
+# hack to override sqlite database filename
+# see: https://help.morph.io/t/using-python-3-with-morph-scraperwiki-fork/148
+os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
 import requests
 import scraperwiki
