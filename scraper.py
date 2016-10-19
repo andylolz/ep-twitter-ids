e6f7f237eaa911ebe50a623b24e38ba9ca694559
Andy Lulham <a.lulham@gmail.com>
2016-10-20 00:57:50 +0100

    Rewrite, mostly (I forgot I’d started this!)

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
-import csv
-from io import StringIO
-import time
+import itertools
 import os
+import time
 
 # hack to override sqlite database filename
 # see: https://help.morph.io/t/using-python-3-with-morph-scraperwiki-fork/148
@@ -10,43 +9,99 @@ os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
 import requests
 import scraperwiki
-from twitter import Twitter, OAuth
 
 
-base_url = 'https://raw.githubusercontent.com/everypolitician/everypolitician-data'
-url = base_url + '/master/countries.json'
-j = requests.get(url).json()
+# TODO: Use git to fetch the data repo
+# (rather than lotsa of requests)
 
-tmpl = base_url + '/{sha}/{path}'
-for country in j:
-    print(country['name'])
-    for legislature in country['legislatures']:
-        leg_handles = {}
-        for period in legislature['legislative_periods']:
-            data = requests.get(tmpl.format(
-                sha=legislature['sha'],
-                path=period['csv'])).text
-            time.sleep(0.5)
-            reader = csv.DictReader(StringIO(data))
-            for x in reader:
-                if x['twitter'] == '':
-                    continue
-                leg_handles[x['id']] = {
-                    'person_id': x['id'],
-                    'country_code': country['code'],
-                    'legislature_slug': legislature['slug'],
-                    'twitter_handle': x['twitter'],
-                }
-        if leg_handles:
-            scraperwiki.sqlite.save(["person_id", "country_code", "legislature_slug"], leg_handles.values(), "data")
+ep_root_url = 'https://cdn.rawgit.com/everypolitician/everypolitician-data/master/'
 
 consumer_key = os.environ.get('MORPH_TWITTER_CONSUMER_KEY')
 consumer_secret = os.environ.get('MORPH_TWITTER_CONSUMER_SECRET')
-access_token = os.environ.get('MORPH_TWITTER_ACCESS_TOKEN')
-access_token_secret = os.environ.get('MORPH_TWITTER_ACCESS_TOKEN_SECRET')
 
-t = Twitter(auth=OAuth(access_token, access_token_secret, consumer_key, consumer_secret))
+def _get_token(consumer_key, consumer_secret):
+    auth = requests.auth.HTTPBasicAuth(consumer_key, consumer_secret)
+    auth_url = 'https://api.twitter.com/oauth2/token'
+    auth_data = {'grant_type': 'client_credentials'}
+    r = requests.post(url=auth_url, data=auth_data, auth=auth)
+    j = r.json()
+    return j['access_token']
 
-# leg_handles = scraperwiki.sqlite.select('* from data')
-handles = [x['twitter_handle'] for x in leg_handles.values()]
-ids = [x['id'] for x in t.users.lookup(screen_name=','.join(handles[:100]), _timeout=1)]
+def _run_query(payload):
+    r = requests.post(
+        'https://api.twitter.com/1.1/users/lookup.json',
+        data=payload,
+        headers=auth_header,
+    )
+    data = r.json()
+    time.sleep(0.5)
+    if r.status_code != 200:
+        if data.get('errors'):
+            for error in data['errors']:
+                print("Error: {msg} ({code})".format(
+                    msg=error['message'],
+                    code=error['code'])
+                )
+        else:
+            print("Error: Some unknown problem")
+        return None
+    return data
+
+# get all the countries
+countries = requests.get(ep_root_url + 'countries.json').json()
+
+twitter_data = {}
+# get the routes to all the popolo files
+for country in countries:
+    for legislature in country['legislatures']:
+        popolo_url = legislature['popolo_url']
+        people = requests.get(popolo_url).json()['persons']
+        time.sleep(0.5)
+        # build a list of twitter handles for every legislature
+        for person in people:
+            twitter_handles = [contact_detail['value'] for contact_detail in person.get('contact_details', []) if contact_detail['type'] == 'twitter']
+            twitter_ids = [identifier['identifier'] for identifier in person.get('identifiers', []) if identifier['scheme'] == 'twitter']
+            for handle, id_ in itertools.zip_longest(twitter_handles, twitter_ids):
+                twitter_data[handle.lower()] = {
+                    'country_code': country['code'],
+                    'legislature_slug': legislature['slug'],
+                    'person_id': person['id'],
+                    'handle': handle,
+                    'twitter_id': id_,
+                }
+
+# auth stuff
+token = _get_token(consumer_key, consumer_secret)
+auth_header = {'Authorization': 'Bearer {token}'.format(token=token)}
+
+# 1. If we have IDs, we want to check handles
+handles_with_ids = [v['twitter_id'] for k, v in twitter_data.items() if v['twitter_id']]
+for lower in range(0, len(handles_with_ids), 100):
+    updates = {}
+    user_ids = ','.join(handles_with_ids[lower:lower + 100])
+    payload = {'user_id': user_ids}
+    data = _run_query(payload)
+    if not data:
+        continue
+    for x in data:
+        handle_lookup = x['screen_name'].lower()
+        updates[handle_lookup] = twitter_data[handle_lookup]
+        updates[handle_lookup]['handle'] = x['screen_name']
+    scraperwiki.sqlite.save(['handle'], updates.values(), "data")
+
+# 2. If we have handles, we want to find the IDs (and check handles!)
+handles_without_ids = [v['handle'] for k, v in twitter_data.items() if not v['twitter_id']]
+for lower in range(0, len(handles_without_ids), 100):
+    updates = {}
+    print('{} to {}'.format(lower, lower + 100))
+    screen_names = ','.join(handles_without_ids[lower:lower + 100])
+    payload = {'screen_name': screen_names}
+    data = _run_query(payload)
+    if not data:
+        continue
+    for x in data:
+        handle_lookup = x['screen_name'].lower()
+        updates[handle_lookup] = twitter_data[handle_lookup]
+        updates[handle_lookup]['twitter_id'] = x['id']
+        updates[handle_lookup]['handle'] = x['screen_name']
+    scraperwiki.sqlite.save(['handle'], updates.values(), "data")
